b0b9ed3c3caebdd70853c7095bbf12e32ac0d9a1
Alanik <152527270+AlanikREDAWN@users.noreply.github.com>
2024-07-22 16:01:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// Kahoot Workshop Starter Repo
+const Kahoot = require("kahoot.js-updated"); 
+const client = new Kahoot();
+let array;
+client.join('07869157', Math.random() + "- KittyCat (Lilia)");
+
+client.on("QuizStart", () => {
+	console.log("The quiz has started!");
+});
+
+client.on("QuizEnd", () => {
+	console.log("The quiz has ended.");
+});
+
+
+client.on("QuestionStart", question => {
+	let finalNo;
+	console.log("New question!");
+	console.log(question.numberOfChoices);
+	// for (let i = 0, i <= question.numberOfChoices)
+	console.log(question.question);
+	// console.log(question);
+	// console.log(question.choices);
+	// console.log(question.choices[0]);
+	// console.log(question.choices[0].correct);
+	// console.log(question.choices.correct);
+
+	question.choices.forEach(function(value, i) {
+		if (question.choices[i].correct === true) {
+			finalNo = i;
+			console.log(finalNo, "is the correct answer");
+		}
+		
+	})
+	console.log(finalNo);
+	question.answer(finalNo);
+
+	// function verifyProperty(array) {
+
+	// 	let finalNo
+	// 	array.forEach(function(value, i) {
+	// 		if ("QuestionStart".choices[value].correct === true) {finalNo = i}
+	// 		console.log(finalNo, "is the correct answer")
+	// 	});
+	// 	return(finalNo)
+	// }
+});
